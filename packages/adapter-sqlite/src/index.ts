24ffe926023243f2e1751d2f486db3357e245915
Ondrej Rafaj <dictator@dictator.local>
2026-08-31 02:20:44 +0100

    Revert "Merge adapter SQLite reference implementation"
    This reverts commit 2003f4bcdc96b9a028759690ff17abb520823559, reversing
    changes made to f71b6fe39128421586dc8b75d8e399d6f50aa093.

--- packages/adapter-sqlite/src/index.ts
+++ packages/adapter-sqlite/src/index.ts
@@ -1,11 +1,2 @@
-export { createSqliteAdapter, SQLITE_PROFILES } from './adapter.ts';
-export { calendarPeriod } from './calendar.ts';
-export { provisionSqliteAdapterStorage } from './ingest.ts';
-export type {
-  CompiledAggregateQuery,
-  CompiledCanonicalIngest,
-  CompiledRecordsQuery,
-  CompiledSemanticQuery,
-  SqliteAdapterOptions,
-  SqliteTextCollation,
-} from './types.ts';
+export {};
+
